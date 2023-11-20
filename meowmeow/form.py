e6cedfa93fcf781ae69a36aff28fd33268581c3f
Kartopili43146865 <104121072+Kartopili43146865@users.noreply.github.com>
2023-11-20 12:39:59 +0400

    meow

--- meowmeow/form.py
+++ meowmeow/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
-from wtforms.fields import StringField, SubmitField, TextAreaField
+from wtforms.fields import SubmitField, TextAreaField
 
 class addPost(FlaskForm):
     title = TextAreaField("let's friend know what's on your mind...")
